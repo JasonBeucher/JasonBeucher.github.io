856d32423c6815b6f0b8f0bd3a07df5c2afe44c7
Jason Beucher <59723132+JasonBeucher@users.noreply.github.com>
2023-06-20 11:17:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function redirect(to){
+    window.location.href = to;
+}
+// console.log("test");
+// //window onload
+// window.onload = function(){
+
+// firstText = document.getElementById("first");
+// secondText = document.getElementById("second");
+// //made an event listener for the first text on hover
+// firstText.addEventListener("mouseover", function(){
+//     //make the text disapear by changing his Y
+//     secondText.style.transform = "translateY(50px)";
+//     secondText.style.display = "block";
+//     setTimeout(function(){
+//         firstText.style.display = "none";
+//         secondText.style.opacity = "1";
+//         secondText.style.transform = "translateY(0)";
+        
+//     },100);
+    
+// }
+// );
+// //made an event listener for when the mouse leaves the first text
+// secondText.addEventListener("mouseout", function(){
+//     secondText.style.transform = "translateY(-100%)";
+//     secondText.style.opacity = "0";
+//     firstText.style.display = "block";
+//     firstText.style.transform = "translateY(-50px)";
+//     setTimeout(function(){
+//         secondText.style.display = "none";
+//         firstText.style.transform = "translateY(0)";
+//         firstText.style.display = "block";
+//     },100);
+// }
+// );
+
+// }
